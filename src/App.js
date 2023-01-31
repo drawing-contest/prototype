c7556327872de26b8861c7ed37eac378a135e2af
Connor McCue <connor.mccue@comcast.net>
2023-01-30 18:54:08 -0800

    changed canvas size

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from 'react';
+// useRef allows persistent values between renders
 
 function App() {
+  const [mouseData, setMouseData] = useState({ x: 0, y: 0 });
+  const canvasRef = useRef(null);
+  const [canvasCTX, setCanvasCTX] = useState(null);
+  const [color, setColor] = useState('#000000');
+  const [size, setSize] = useState(10);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    // console.log(canvasCTX);
+    const ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    console.log(ctx);
+    setCanvasCTX(ctx);
+  }, [canvasRef]);
+
+  const setPos = (e) => {
+    setMouseData({
+      x: e.clientX,
+      y: e.clientY,
+    });
+  };
+  // getImageData(0,0,100)
+  // pass canvas.width and canvas.height
+  const draw = (e) => {
+    if (e.buttons !== 1) return;
+    const ctx = canvasCTX;
+    console.log(ctx);
+    ctx.beginPath();
+    ctx.moveTo(mouseData.x, mouseData.y);
+    setMouseData({
+      x: e.clientX,
+      y: e.clientY,
+    });
+    ctx.lineTo(e.clientX, e.clientY);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = size;
+    // Set the line cap to round
+    ctx.lineCap = 'round';
+    ctx.stroke();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div>
+        Welcome to my game!
+      </div>
+      <div>
+        <canvas
+          ref={canvasRef}
+          onMouseEnter={(e) => setPos(e)}
+          onMouseMove={(e) => draw(e)}
+          onMouseDown={(e) => setPos(e)}
+        ></canvas>
+        <div
+          // className='controlpanel'
+          style={{
+            position: 'absolute',
+            top: '0',
+            left: '0',
+            width: '100%',
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <input
+            type="range"
+            value={size}
+            max={40}
+            onChange={(e) => {
+              setSize(e.target.value);
+            }}
+          />
+          <input
+            type="color"
+            value={color}
+            onChange={(e) => {
+              setColor(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              const ctx = canvasCTX;
+              ctx.clearRect(
+                0,
+                0,
+                canvasRef.current.width,
+                canvasRef.current.height
+              );
+            }}
+          >
+                    Clear
+          </button>
+        </div>
+      </div>
+    </>
+
   );
 }
 
