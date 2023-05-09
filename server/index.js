2ecc252805e1c451c13f4e5e5bd31ac42c8167e1
Ryan J. Parker <ryan.jos.parker@gmail.com>
2023-05-09 11:28:27 -0700

    update server origin to deployed url

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://singular-dusk-aa838f.netlify.app' || 'http://localhost:3000',
     methods: ['GET', 'POST'],
   },
 });
